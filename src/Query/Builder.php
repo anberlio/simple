037e52cb4da67b1d2e4cfa1061e0220c6f01f53a
ray <ray123@qq.com>
2024-05-27 16:09:08 +0800

    去掉批量插入

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -560,7 +560,7 @@ class Builder
         $this->sql = $body;
         try {
             $result = $this->client->bulk($this->sql);
-            if(isset($result['items'][0]['index']['status']) &&  ($result['items'][0]['index']['status']!=201 ||$result['items'][0]['index']['status']!=200)){
+            if(empty($result['items']) || $result['errors']){
                 $this->logger->error('批量插入错误insertErrorInside',[
                     'message'=>$result,
                 ]);
